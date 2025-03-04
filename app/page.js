958a118be3b1e9d6a2f8547cc9c30e8c6677f401
abhishek <abhishekjaswal1122@gmail.com>
2025-03-04 17:40:27 +0530

    added css file and rest files

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import VitalsInput from "./components/VitalsInput";
 import Report from "./components/Report";
 import MealRecommendation from "./components/MealRecommendation";
+import "./globals.css";
 
 export default function Home() {
   const [name, setName] = useState("");
@@ -37,21 +38,36 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-green-50 to-beige-100 p-6">
-      <div className="max-w-1xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
-        <VitalsInput 
-          name={name} setName={setName} 
-          height={height} setHeight={setHeight} 
-          weight={weight} setWeight={setWeight} 
-          dob={dob} setDob={setDob} 
-          gender={gender} setGender={setGender} 
-          activity={activity} setActivity={setActivity} 
-          goal={goal} setGoal={setGoal} 
-          calculateValues={calculateValues} 
-        />
-         <div className="min-h-screen bg-gradient-to-br from-green-50 to-beige-100 p-6">
-        <Report name={name} dob={dob} bmi={bmi} bmr={bmr} tdee={tdee} weight={weight} goal={goal} />
-        <MealRecommendation tdee={tdee} />
+      {/* Parent container: Flexbox layout */}
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row gap-6">
+        
+        {/* Left Container (VitalsInput) */}
+        <div className="flex-1 bg-white p-6 rounded-lg shadow-md">
+          <VitalsInput 
+            name={name} setName={setName} 
+            height={height} setHeight={setHeight} 
+            weight={weight} setWeight={setWeight} 
+            dob={dob} setDob={setDob} 
+            gender={gender} setGender={setGender} 
+            activity={activity} setActivity={setActivity} 
+            goal={goal} setGoal={setGoal} 
+            calculateValues={calculateValues} 
+          />
         </div>
+
+        {/* Right Container (Splitting into two sections) */}
+        <div className="flex-1 flex flex-col gap-6">
+          {/* Report Section */}
+          <div className="bg-white p-6 rounded-lg shadow-md flex-1">
+            <Report name={name} dob={dob} bmi={bmi} bmr={bmr} tdee={tdee} weight={weight} goal={goal} />
+          </div>
+
+          {/* Meal Recommendation Section */}
+          <div className="bg-white p-6 rounded-lg shadow-md flex-1">
+            <MealRecommendation  />
+          </div>
+        </div>
+
       </div>
     </div>
   );
